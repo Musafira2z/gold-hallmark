de502d4b741950ae5624dddd8592a4f88bcb454a
sayedsafi2000 <safiuddin102030@gmail.com>
2025-01-22 14:09:13 +0600

    FIx item name and item deletations

--- client/src/pages/Invoice.jsx
+++ client/src/pages/Invoice.jsx
@@ -1,29 +1,36 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import "./print.css"
 import { apiUrl } from "../context/UserContext.jsx";
 const Invoice = () => {
     const { id } = useParams(); // Get the order ID from the URL
     const [order, setOrder] = useState(null);
+    const didPrint = useRef(false);
 
     useEffect(() => {
         const fetchOrderData = async () => {
             try {
                 const response = await axios.get(`${apiUrl}/orders/${id}`);
-                setOrder(response.data);
-                const image = new Image();
-                image.src = `${apiUrl}${response.data.image}`;
-                image.onload = function () {
-                    window.print();
-                    window.onafterprint = () => {
-                        window.close();
-                    };
-                };
+                setOrder(response.data); // Update the order state
+
+                // Add this condition check before the print operation
+                if (!didPrint.current) {
+                    didPrint.current = true;
+
+                    // Wait for a short delay to ensure the DOM renders
+                    setTimeout(() => {
+                        window.print();
+                        window.onafterprint = () => {
+                            window.close();
+                        };
+                    }, 500); // Allow DOM to render before printing
+                }
             } catch (error) {
                 console.error("Error fetching data:", error);
             }
         };
+
         fetchOrderData();
     }, [id]);
 
@@ -33,65 +40,65 @@ const Invoice = () => {
     }
     return (
         <div className="bg-[#E0F2F1] h-[100vh] pt-20 print-content">
-            <h2 className="text-center pt-10 font-semibold text-2xl">Hall Mark Cash Memo</h2>
-            <div className="bg-white px-20 rounded-lg mt-4 max-w-6xl mx-auto">
+            <h2 className="text-center pt-5 font-semibold text-xl">Hall Mark Cash Memo</h2>
+            <div className="bg-white px-20 rounded-lg mt-2 max-w-6xl mx-auto">
                 <h2 className="text-lg font-bold">Customer Profile</h2>
                 <div className="grid grid-cols-3 gap-2 mb-0 items-center">
                     <div>
-                        <p className="text-[12px]">
+                        <p className="text-xs">
                             <span className="">ID:</span> {order?.customerID}
                         </p>
-                        <p className="text-[12px]">
+                        <p className="text-xs">
                             <span className="">Name:</span> {order?.name}
                         </p>
-                        <p className="text-[12px]">
+                        <p className="text-xs">
                             <span className="">Mobile:</span> {order?.contact}
                         </p>
-                        <p className="text-[12px]">
+                        <p className="text-xs">
                             <span className="">Address:</span> {order?.address}
                         </p>
                     </div>
                     <div>
-                        <p className="text-[12px]">
+                        <p className="text-xs">
                             <span className="">Company:</span> {order?.company}
                         </p>
-                        <p className="text-[12px]">
+                        <p className="text-xs">
                             <span className="">Voucher Number:</span> {order?.voucher}
                         </p>
-                        <p className="text-[12px]">
+                        <p className="text-xs">
                             <span className="">Delivery Date:</span> {order?.createdAt ? new Date(order.createdAt).toLocaleDateString('en-GB') : 'N/A'}
                         </p>
-                        <p className="text-[12px]">
+                        <p className="text-xs">
                             <span className="">Delivery Time:</span>
                             {order?.createdAt ? new Date(order.createdAt).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: true }) : 'N/A'}
                         </p>
                     </div>
                     <div className="w-full flex justify-end items-start ">
-                        <img className="" src={`${apiUrl}${order.image}`} alt="" />
+                        <img className="w-7/12" src={`${apiUrl}${order.image}`} alt="" />
                     </div>
                 </div>
 
-                <h2 className="text-lg font-bold mb-2">Order Details</h2>
+                <h2 className="text-md font-bold mb-1">Order Details</h2>
                 <table className="w-full border-collapse border border-gray-300 text-left">
                     <thead className="bg-gray-100">
                         <tr>
-                            <th className="border border-gray-300 px-4 py-2">Item Name</th>
-                            <th className="border border-gray-300 px-4 py-2">Quantity</th>
-                            <th className="border border-gray-300 px-4 py-2">Rate (BDT)</th>
-                            <th className="border border-gray-300 px-4 py-2">Weight</th>
-                            <th className="border border-gray-300 px-4 py-2">Hall Mark</th>
-                            <th className="border border-gray-300 px-4 py-2">Amount</th>
+                            <th className="border border-gray-300 text-sm px-4 py-2">Item Name</th>
+                            <th className="border border-gray-300 text-sm px-4 py-2">Quantity</th>
+                            <th className="border border-gray-300 text-sm px-4 py-2">Rate (BDT)</th>
+                            <th className="border border-gray-300 text-sm px-4 py-2">Weight</th>
+                            <th className="border border-gray-300 text-sm px-4 py-2">Hall Mark</th>
+                            <th className="border border-gray-300 text-sm px-4 py-2">Amount</th>
                         </tr>
                     </thead>
                     <tbody>
                         {order.items.map((item, index) => ( // Map over items array
                             <tr key={index}>
-                                <td className="border border-gray-300 px-4 py-0">{item.item}</td>
-                                <td className="border border-gray-300 px-4 py-0">{item.quantity}</td>
-                                <td className="border border-gray-300 px-4 py-0">{item.rate}</td>
-                                <td className="border border-gray-300 px-4 py-0">{item.weight} {item.weightUnite}</td>
-                                <td className="border border-gray-300 px-4 py-0">{item.xray}</td> {/* Assuming xray applies to all items */}
-                                <td className="border border-gray-300 px-4 py-0">{item.amount}</td>
+                                <td className="border border-gray-300 px-4 text-xs py-0">{item.item}</td>
+                                <td className="border border-gray-300 px-4 text-xs py-0">{item.quantity}</td>
+                                <td className="border border-gray-300 px-4 text-xs py-0">{item.rate}</td>
+                                <td className="border border-gray-300 px-4 text-xs py-0">{item.weight} {item.weightUnite}</td>
+                                <td className="border border-gray-300 px-4 text-xs py-0">{item.xray}</td> {/* Assuming xray applies to all items */}
+                                <td className="border border-gray-300 px-4 text-xs py-0">{item.amount}</td>
                             </tr>
                         ))}
                     </tbody>
@@ -99,21 +106,21 @@ const Invoice = () => {
 
                 <div className="flex justify-between items-center mt-1">
                     <div>
-                        <p>
-                            <span className="">Total Amount:</span> {order.totalAmount} BDT
+                        <p className="text-sm font-bold">
+                            <span className="text-xs font-normal">Total Amount:</span> {order.totalAmount} BDT
                         </p>
-                        <p>
-                            <span className="">Paid Amount:</span> {order.totalAmount} BDT
+                        <p className="text-sm font-bold">
+                            <span className="text-xs font-normal">Paid Amount:</span> {order.totalAmount} BDT
                         </p>
                     </div>
                 </div>
 
                 <div className="flex justify-between items-center mt-6">
                     <div className="border-t border-gray-300 text-center px-4">
-                        <p>Customer Signature</p>
+                        <p className="text-sm">Customer Signature</p>
                     </div>
                     <div className="border-t border-gray-300 text-center px-4">
-                        <p>Authorized Signature</p>
+                        <p className="text-sm">Authorized Signature</p>
                     </div>
                 </div>
             </div>
